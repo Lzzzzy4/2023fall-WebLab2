5dd1d4d2a69ad9bd2e13bff93e52f71ba1a1e159
Lzzzzy4 <lzy1522659928@mail.ustc.edu.cn>
2023-12-21 17:02:47 +0800

    fix readdata

--- src/main.py
+++ src/main.py
@@ -4,10 +4,9 @@ import os
 path = os.path.dirname(__file__)
 
 movie_tag = pd.read_csv(path+'/../data/Movie_tag.csv')
-movie_tag = movie_tag.set_index('id')['tag']
-# print(movie_tag[1291543])
-
 douban2fb = pd.read_csv(path+'/../data/douban2fb.csv')
-douban2fb = douban2fb.set_index('id')['entity']
-# print(douban2fb.head())
+movie = pd.merge(movie_tag,douban2fb,how='inner',on='id')
+# print(movie.head(5))
+
+
 
