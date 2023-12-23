63f27eaa6409ba7e0a49a803beeeaed01a2f37f4
Dashes <fangchizheng@mail.ustc.edu.cn>
2023-12-23 15:18:44 +0800

    stage1 fast fast

--- src/1step.py
+++ src/1step.py
@@ -8,25 +8,28 @@ import datetime
 
 path = os.path.dirname(__file__)
 
-movie_tag = pd.read_csv(path+'/../data/Movie_tag.csv')
-douban2fb = pd.read_csv(path+'/../data/douban2fb.csv')
-df = pd.merge(movie_tag,douban2fb,how='inner',on='id')
-df.set_index('entity',inplace=True)
+movie_tag = pd.read_csv(path + '/../data/Movie_tag.csv')
+douban2fb = pd.read_csv(path + '/../data/douban2fb.csv')
+df = pd.merge(movie_tag, douban2fb, how='inner', on='id')
+df.set_index('entity', inplace=True)
 df['tag'] = df['tag'].apply(lambda x: x.split(','))
 # print(df.head(5))
 
-df.to_json(orient='index', path_or_buf=path+'/../data/movie_entity.json', force_ascii=False, indent=4)
+df.to_json(orient='index',
+           path_or_buf=path + '/../data/movie_entity.json',
+           force_ascii=False,
+           indent=4)
 
 movie_list = set(df.index.tolist())
-graph = copy.deepcopy( df.drop(['id','tag'],axis=1) )
-graph.insert(loc = 0,column = 'is_movie', value = 1)
-graph.insert(loc = 1,column = 'count', value = 0)
-graph.insert(loc = 2,column = 'content', value = None)
+graph = copy.deepcopy(df.drop(['id', 'tag'], axis=1))
+graph.insert(loc=0, column='is_movie', value=1)
+graph.insert(loc=1, column='count', value=0)
+graph.insert(loc=2, column='content', value=None)
 graph['content'] = graph['content'].apply(lambda x: [])
 # print(graph.head(5))
 
 st = time.time()
-with gzip.open(path+'/../data/freebase_douban.gz', 'rb') as f:
+with gzip.open(path + '/../data/freebase_douban.gz', 'rb') as f:
     i = 0
     cnt = 0
     for line in f:
@@ -34,48 +37,53 @@ with gzip.open(path+'/../data/freebase_douban.gz', 'rb') as f:
         i = i + 1
         line = line.strip()
         triplet = line.decode().split('\t')[:3]
-        if(i % 1000000 == 1):
+        if (i % 1000000 == 1):
             # print(triplet)
-            print(i/395577070 * 100 , '%')
-            last_time =(1-i/395577070) * (time.time() - st)/i * 395577070 
-            print("剩余时间：",datetime.timedelta(seconds=last_time))
+            print(i / 395577070 * 100, '%')
+            last_time = (1 - i / 395577070) * (time.time() -
+                                               st) / i * 395577070
+            print("剩余时间：", datetime.timedelta(seconds=last_time))
             print(cnt)
             cnt = 0
+        continue
         # if(i == 10000000):
         #     break
-        
+
         patten = "<http://rdf.freebase.com/ns/"
-        if(patten != triplet[0][:len(patten)] or patten != triplet[2][:len(patten)]):
+        if (patten != triplet[0][:len(patten)]
+                or patten != triplet[2][:len(patten)]):
             continue
         item1 = triplet[0][len(patten):-1]
         item2 = triplet[2][len(patten):-1]
         relation = triplet[1]
 
         # 一跳子图
-        if(item1 in movie_list):
+        if (item1 in movie_list):
             cnt = cnt + 1
-            graph.loc[item1,'content'].append((relation,item2))
-            graph.loc[item1,'count'] = graph.loc[item1,'count'] + 1
-        if(item2 in movie_list):
+            graph.loc[item1, 'content'].append((relation, item2))
+            graph.loc[item1, 'count'] = graph.loc[item1, 'count'] + 1
+        if (item2 in movie_list):
             cnt = cnt + 1
             if (item1 not in graph.index):
-                graph.loc[item1] = [0,0,[]]
-            graph.loc[item1,'content'].append((relation,item2))
-            graph.loc[item1,'count'] = graph.loc[item1,'count'] + 1
+                graph.loc[item1] = [0, 0, []]
+            graph.loc[item1, 'content'].append((relation, item2))
+            graph.loc[item1, 'count'] = graph.loc[item1, 'count'] + 1
 
-    print(i) #395577070
+    print(i)  #395577070
     # 过滤
-    graph.insert(loc = 3,column = 'delete', value = 0)
+    graph.insert(loc=3, column='delete', value=0)
     for index, row in graph.iterrows():
-        if(row['is_movie'] == 0 and row['count'] < 20):
-            graph.loc[index,'delete'] = 1
+        if (row['is_movie'] == 0 and row['count'] < 20):
+            graph.loc[index, 'delete'] = 1
             continue
 
     graph = graph[graph['delete'] == 0]
-    graph.drop(['delete'],axis=1,inplace=True)
+    graph.drop(['delete'], axis=1, inplace=True)
 
-    graph.to_json(orient='index', path_or_buf=path+'/../data/movie_graph.json', force_ascii=False, indent=4)
+    graph.to_json(orient='index',
+                  path_or_buf=path + '/../data/movie_graph.json',
+                  force_ascii=False,
+                  indent=4)
 
     end = time.time()
-    print("用时：",datetime.timedelta(seconds=end-st))
-        
+    print("用时：", datetime.timedelta(seconds=end - st))
--- src/2step.py
+++ src/2step.py
@@ -3,18 +3,21 @@ import os
 import gzip
 import copy
 import json
-import keyboard
 import datetime
 import time
 import multiprocessing as mp
 
 path = os.path.dirname(__file__)
 
-graph = pd.read_json(path+'/../data/movie_graph.json',encoding="utf-8", orient='index')
+graph = pd.read_json(path + '/../data/movie_graph.json',
+                     encoding="utf-8",
+                     orient='index')
 step1_list = graph[graph['is_movie'] == 0].index.tolist()
 step1_list = set(step1_list)
-graph.insert(loc = 3,column = 'is_first_step', value = 1)
-print(graph.head(5))
+graph.insert(loc=3, column='is_first_step', value=1)
+
+#graph to dict
+# print(graph.head(5))
 
 # 统计graph中所有关系的数量，过滤
 relation_count = {}
@@ -26,86 +29,83 @@ for index, row in graph.iterrows():
 
 delete_list = []
 for key in relation_count.keys():
-    if(relation_count[key] < 500):
+    if (relation_count[key] < 500):
         delete_list.append(key)
 for i in delete_list:
     relation_count.pop(i)
 
 relation_list = list(relation_count.keys())
 relation_list = set(relation_list)
-print(len(relation_list))
+print(len(step1_list), len(relation_list), len(graph))
 
 st = time.time()
-with gzip.open(path+'/../data/freebase_douban.gz', 'rb') as f:
+
+graph = graph.to_dict(orient='index')
+with gzip.open(path + '/../data/freebase_douban.gz', 'rb') as f:
     i = 0
     cnt = 0
     for line in f:
-        # break
-        if keyboard.is_pressed('l'):
-            break
         i = i + 1
-        # if(i%2 == 0):
-        #     continue
 
         line = line.strip()
         triplet = line.decode().split('\t')[:3]
-        if(i % 100000 == 0):
+        if (i % 100000 == 0):
 
-            print(i/395577070 * 100 , '%')
-            last_time =(1-i/395577070) * (time.time() - st)/i * 395577070 
-            print("剩余时间：",datetime.timedelta(seconds=last_time))
+            print(i / 395577070 * 100, '%')
+            last_time = (1 - i / 395577070) * (time.time() -
+                                               st) / i * 395577070
+            print("剩余时间：", datetime.timedelta(seconds=last_time))
             print(cnt)
+            print(len(graph))
             cnt = 0
-        
+
         patten = "<http://rdf.freebase.com/ns/"
 
         relation = triplet[1]
-        if(relation not in relation_list):
+        if (relation not in relation_list):
             continue
 
-        if(patten not in triplet[0] or patten not in triplet[2]):
+        if (patten not in triplet[0] or patten not in triplet[2]):
             continue
         item1 = triplet[0].split(patten)[1][:-1]
         item2 = triplet[2].split(patten)[1][:-1]
 
-        if(item1 in step1_list):
+        if (item1 in step1_list):
             cnt = cnt + 1
-            if relation not in graph.loc[item1,'content']:
-                graph.loc[item1,'content'][relation] = []
-            graph.loc[item1,'content'][relation].append(item2)
-            graph.loc[item1,'count'] = graph.loc[item1,'count'] + 1
-        if(item2 in step1_list):
+            if relation not in graph[item1]['content']:
+                graph[item1]['content'][relation] = []
+            graph[item1]['content'][relation].append(item2)
+            graph[item1]['count'] += 1
+        if (item2 in step1_list):
             cnt = cnt + 1
-            if (item1 not in graph.index):
-                graph.loc[item1] = [0,0,{},0]
-            if relation not in graph.loc[item1,'content']:
-                graph.loc[item1,'content'][relation] = []
-            graph.loc[item1,'content'][relation].append(item2)
-            graph.loc[item1,'count'] = graph.loc[item1,'count'] + 1
+            if (item1 not in graph):
+                # graph.loc[item1] = [0, 0, {}, 0]
+                graph[item1] = {
+                    'is_movie': 0,
+                    'count': 0,
+                    'content': {},
+                    'is_first_step': 0
+                }
+            if relation not in graph[item1]['content']:
+                graph[item1]['content'][relation] = []
+            graph[item1]['content'][relation].append(item2)
+            graph[item1]['count'] += 1
 
     print(i)
     # 过滤
-    graph.insert(loc = 4,column = 'delete', value = 0)
+    graph.insert(loc=4, column='delete', value=0)
     for index, row in graph.iterrows():
-        if(row['is_first_step'] == 0 and row['count'] < 30):
-            graph.loc[index,'delete'] = 1
+        if (row['is_first_step'] == 0 and row['count'] < 30):
+            graph.loc[index, 'delete'] = 1
             continue
         # for key in row['content'].keys():
         #     if(len(row['content'][key]) < 3):
         #         row['content'].pop(key)
 
     graph = graph[graph['delete'] == 0]
-    graph.drop(['delete'],axis=1,inplace=True)
+    graph.drop(['delete'], axis=1, inplace=True)
 
-    graph.to_json(orient='index', path_or_buf='E:/1.json', force_ascii=False, indent=4)
-        
-            
-
-
-
-        
-
-
-
-
-
+    graph.to_json(orient='index',
+                  path_or_buf='E:/1.json',
+                  force_ascii=False,
+                  indent=4)
